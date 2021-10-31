5738680e58c0b35e890d625df55aa81fdf2f2abf
Andrew Walbran <qwandor@google.com>
2021-10-31 20:45:27 +0000

    Trying to get output to oscilloscope

--- examples/receive.rs
+++ examples/receive.rs
@@ -1,12 +1,16 @@
 use anyhow::Result;
 use linux_embedded_hal::spidev::{SpiModeFlags, SpidevOptions};
 use linux_embedded_hal::SpidevDevice;
+use rfm69::registers::{
+    DataMode, DioMapping, DioMode, DioPin, DioType, InterPacketRxDelay, Modulation,
+    ModulationShaping, ModulationType, PacketConfig, PacketDc, PacketFiltering, PacketFormat,
+};
 use rfm69::Rfm69;
 use utilities::rfm_error;
 
 fn main() -> Result<()> {
     // Configure SPI 8 bits, Mode 0
-    let mut spi = SpidevDevice::open("/dev/spidev0.0")?;
+    let mut spi = SpidevDevice::open("/dev/spidev0.1")?;
     let options = SpidevOptions::new()
         .bits_per_word(8)
         .max_speed_hz(1_000_000)
@@ -22,12 +26,41 @@ fn main() -> Result<()> {
         println!("Register 0x{:02x} = 0x{:02x}", index + 1, val);
     }
 
+    rfm_error!(rfm.frequency(433_920_000.0))?;
+    rfm_error!(rfm.bit_rate(55_555.0))?;
+    rfm_error!(rfm.preamble(0))?;
+    rfm_error!(rfm.sync(&[]))?;
+
+    rfm_error!(rfm.modulation(Modulation {
+        data_mode: DataMode::Continuous,
+        modulation_type: ModulationType::Ook,
+        shaping: ModulationShaping::Shaping00,
+    }))?;
+    rfm_error!(rfm.packet(PacketConfig {
+        format: PacketFormat::Variable(10),
+        dc: PacketDc::None,
+        filtering: PacketFiltering::None,
+        crc: false,
+        interpacket_rx_delay: InterPacketRxDelay::Delay2Bits,
+        auto_rx_restart: true,
+    }))?;
+    rfm_error!(rfm.dio_mapping(DioMapping {
+        pin: DioPin::Dio2,
+        dio_type: DioType::Dio01,
+        dio_mode: DioMode::Rx,
+    }))?;
+    rfm_error!(rfm.dio_mapping(DioMapping {
+        pin: DioPin::Dio5,
+        dio_type: DioType::Dio00,
+        dio_mode: DioMode::Rx,
+    }))?;
+
     // Prepare buffer to store the received data
     let mut buffer = [0; 64];
     rfm_error!(rfm.recv(&mut buffer))?;
     // Print received data
     for (index, val) in buffer.iter().enumerate() {
-        println!("Value at {} = {}", index, val)
+        println!("Value at {} = {}", index, val);
     }
 
     Ok(())
